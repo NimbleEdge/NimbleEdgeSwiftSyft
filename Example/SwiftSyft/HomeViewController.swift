b5141dc42e987c5b3b9fff2a99a4e9efeae3e7fc
mjjimenez <mjjimenez87@gmail.com>
2022-08-16 11:41:07 +0800

    Added way for client to supply their own device identifier

--- Example/SwiftSyft/HomeViewController.swift
+++ Example/SwiftSyft/HomeViewController.swift
@@ -93,7 +93,7 @@ class HomeViewController: UIViewController, UITextViewDelegate {
             self.syftJob = syftClient.newJob(modelName: "mnist",
                                              version: "1.0",
                                              inference: true,
-                                             loggingClientToken: "pub7eb28d4902814586c6e56e0b67a58f9c")
+                                             loggingClientToken: "pubfe47ad8f1c5a4f21e498b46f87c157ac")
 
             // This function is called when SwiftSyft has downloaded the plans and model parameters from PyGrid
             // You are ready to train your model on your data
